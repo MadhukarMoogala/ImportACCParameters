028b75fb984b72b1844600370be2a521f456a64d
Madhukar Moogala <madhukar.moogala@autodesk.com>
2023-07-09 14:42:26 +0530

    Add project files.

---
+++ Entry.cs
@@ -0,0 +1,306 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Autodesk.Forge.Model;
+using Autodesk.Forge;
+using System.Threading;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Windows.OPM;
+using System.Runtime.InteropServices;
+using Autodesk.AutoCAD.LayerManager;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Interop.Common;
+using System.Runtime.InteropServices.ComTypes;
+using System.Net.Http;
+
+[assembly: ExtensionApplication(typeof(ImportACCParameters.Plugin))]
+[assembly: CommandClass(typeof(ImportACCParameters.Entry))]
+
+namespace ImportACCParameters
+{
+
+    [Guid("D029DA19-78A0-41FE-86A2-E5FBA9FC73DA"),
+     ProgId("OPMNetSample.CustomProperty.1"), 
+     ClassInterface(ClassInterfaceType.None),
+     ComDefaultInterface(typeof(IDynamicProperty2)), 
+     ComVisible(true)]
+    public class AssetProperty : IDynamicProperty2
+    {
+        private IDynamicPropertyNotify2 m_pSink = null;
+        public void GetGUID(out Guid propGUID)
+        {
+            propGUID = new Guid("D029DA19-78A0-41FE-86A2-E5FBA9FC73DA");
+        }
+        public void GetDisplayName(out string szName)
+        {
+            szName = Entry.FetchParameterName;
+        }
+        public void IsPropertyEnabled(object pUnk, out int bEnabled)
+        {
+            bEnabled = 1;
+        }
+        public void IsPropertyReadOnly(out int bReadonly)
+        {
+            bReadonly = 1;
+        }
+        public void GetDescription(out string szName)
+        {
+            szName = Entry.FetchParameterDescription;
+        }
+        public void GetCurrentValueName(out string szName)
+        {
+            throw new System.NotImplementedException();
+        }
+        public void GetCurrentValueType(out ushort varType)
+        {
+            varType = 3;
+        }
+        public void GetCurrentValueData(object pUnk, ref object pVarData)
+        {
+            if (pUnk is AcadObject obj)
+            {
+                Document doc =
+                  Application.DocumentManager.MdiActiveDocument;
+                Transaction tr =
+                  doc.TransactionManager.StartTransaction();
+                using (tr)
+                {
+                    DBObject o = tr.GetObject(
+                        new ObjectId((IntPtr)obj.ObjectID),
+                        OpenMode.ForRead
+                      );
+                    pVarData = Entry.FetchAssetIdFromParameters(o);
+                }
+            }
+            else
+                pVarData = 0;
+        }
+
+       
+
+        public void SetCurrentValueData(object pUnk, object varData)
+        {
+        }
+        public void Connect(object pSink)
+        {
+            m_pSink = (IDynamicPropertyNotify2)pSink;
+        }
+        public void Disconnect()
+        {
+            m_pSink = null;
+        }
+    }
+
+   
+    public class Forge
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+
+    public class ForgeConfiguration
+    {
+        public Forge Forge { get; set; }
+    }
+    public class Entry
+    {
+        public static Random randomInt  =>   new Random();
+        public static string BearerToken = string.Empty;    
+        private static string ParameterName {  get; set; }
+        private static string ParameterDescription { get; set; }
+        public static string FetchParameterName
+        {
+            get
+            {
+                return ParameterName;
+            }
+        }
+
+        public static string FetchParameterDescription
+        {
+            get 
+            { 
+                return ParameterDescription; 
+            } 
+        }
+        public static long FetchAssetIdFromParameters(DBObject dbEntity)
+        {
+            var entType = dbEntity.Id.ObjectClass.Name;
+            long assetId;
+            switch (entType)
+            {
+                case "AcDbBlockReference":
+                    {
+                        assetId = randomInt.Next();
+                        break;
+                    }
+                case "AcDbLine":
+                    {
+                        assetId = randomInt.Next();                        
+                        break;
+                    }
+                default:
+                    {
+                        assetId = default;                        
+                        break;
+                    }
+            }
+
+            return assetId;
+        }
+
+        public static void Print(string msg)
+        {
+            var doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            if (doc == null) return;
+            var ed = doc.Editor;
+            ed.WriteMessage($"{msg}\n");
+        }
+        public static string GetAssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+        public static string Login()
+        {
+            //Create a JSON file with Forge App credentials.
+            /*{
+                "Forge": {
+                    "ClientId": "",
+                    "ClientSecret": ""
+                }
+            }*/
+
+            var forgeConfiguration = JsonConvert.DeserializeObject<ForgeConfiguration>(
+                File.ReadAllText(
+                    Path.Combine(GetAssemblyDirectory,
+                    "appsettings.user.json")));
+
+            var oAuthHandler = OAuthHandler.Create(forgeConfiguration.Forge);
+            string token = string.Empty;
+            //We want to sleep the thread until we get 3L access_token.
+            //https://stackoverflow.com/questions/6306168/how-to-sleep-a-thread-until-callback-for-asynchronous-function-is-received
+            AutoResetEvent stopWaitHandle = new AutoResetEvent(false);
+            oAuthHandler.Invoke3LeggedOAuth(async (bearer) =>
+            {
+                // This is our application delegate. It is called upon success or failure
+                // after the process completed
+                if (bearer == null)
+                {
+                    Console.Error.WriteLine("Sorry, Authentication failed!", "3legged test");
+                    return;
+                }
+                token = bearer.access_token;
+                // The call returned successfully and you got a valid access_token.                
+                DateTime dt = DateTime.Now;
+                dt.AddSeconds(double.Parse(bearer.expires_in.ToString()));
+                UserProfileApi profileApi = new UserProfileApi();
+                profileApi.Configuration.AccessToken = bearer.access_token;
+                DynamicJsonResponse userResponse = await profileApi.GetUserProfileAsync();
+                UserProfile user = userResponse.ToObject<UserProfile>();
+                Print($"Hello {user.FirstName} !!, You are Logged in!");            
+                stopWaitHandle.Set();
+            });
+            stopWaitHandle.WaitOne();
+            return token;
+
+        }
+        [CommandMethod("ACCLOGIN")]
+        public static void AccLogin()
+        {
+            BearerToken = Login();
+            Print($"Bearer Token:{BearerToken}");                          
+                
+        }
+        [CommandMethod("IMPORTPARAMETERS")]
+        public static void ImportParameters()
+        {
+            var doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            if (doc == null) return;
+            var ed = doc.Editor;
+            List<ParametersResult> parameters;
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://developer.api.autodesk.com/parameters/v1/");
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {BearerToken}");
+                ed.WriteMessage("\nParameters Import Begin");
+                parameters =  ParameterHandler.ParametersServiceRequest(client);
+                ed.WriteMessage("\nParameters Import End");
+            }
+
+            ed.PointMonitor += (object sender, PointMonitorEventArgs e) =>
+            {
+                FullSubentityPath[] paths = e.Context.GetPickedEntities();
+
+                foreach (FullSubentityPath path in paths)
+                {
+                    ObjectId id = path.GetObjectIds()[0];
+                    switch (id.ObjectClass.Name)
+                    {
+                        case "AcDbBlockReference":
+                            {
+                                ParameterName = parameters[1].Name;
+                                ParameterDescription = parameters[1].Description;
+                                break;
+                            }
+                        case "AcDbLine":
+                            {
+                                ParameterName = parameters[0].Name;
+                                ParameterDescription = parameters[0].Description;
+                                break;
+                            }
+                        default:
+                            {                               
+                                ParameterName = default;
+                                break;
+                            }
+                    }
+                }
+            };
+
+        }
+        
+
+
+    }
+
+    public class Plugin : IExtensionApplication
+    {
+        protected internal AssetProperty assetProperty = null;
+        public void Initialize()
+        {
+            Assembly.LoadFrom("OPMNetExt.dll");
+            Dictionary classDict = SystemObjects.ClassDictionary;
+            RXClass block = (RXClass)classDict.At("AcDbBlockReference");
+            RXClass line = (RXClass)classDict.At("AcDbLine");
+            assetProperty = new AssetProperty();
+            IPropertyManager2 pPropMan = (IPropertyManager2)xOPM.xGET_OPMPROPERTY_MANAGER(block);            
+            pPropMan.AddProperty(assetProperty);
+            pPropMan = (IPropertyManager2)xOPM.xGET_OPMPROPERTY_MANAGER(line);           
+            pPropMan.AddProperty(assetProperty);
+            
+        }
+        public void Terminate()
+        {
+            Dictionary classDict = SystemObjects.ClassDictionary;
+            RXClass block = (RXClass)classDict.At("AcDbBlockReference");
+            RXClass line = (RXClass)classDict.At("AcDbLine");
+            IPropertyManager2 pPropMan = (IPropertyManager2)xOPM.xGET_OPMPROPERTY_MANAGER(block);
+            pPropMan.RemoveProperty((object)assetProperty);
+            pPropMan = (IPropertyManager2)xOPM.xGET_OPMPROPERTY_MANAGER(line);
+            assetProperty = null;
+        }
+    }
+}
---
+++ ParameterHandler.cs
@@ -0,0 +1,159 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace ImportACCParameters
+{
+    internal class ParameterHandler
+    {
+        private static readonly string accountId = "489c5e7a-c6c0-4212-81f3-3529a621210b";
+        private static readonly string groupName = "My 3nd Sample Group";
+        private static readonly string collectionName = "ACC-AutoCAD-Parameters";
+        public static List<ParametersResult> ParametersServiceRequest(HttpClient client)
+        {
+
+            var task = Task.Run(async () =>
+            {
+                return await ParametersServiceRequestAsync(client);
+            });
+            return task.GetAwaiter().GetResult();
+
+        }
+
+        public static async Task<List<ParametersResult>> ParametersServiceRequestAsync(HttpClient client)
+        {
+            var groupId = string.Empty;
+            var collectionId = string.Empty;
+            List<ParametersResult> parametersList;
+            var groupRequest = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(client.BaseAddress,$"accounts/{accountId}/groups")               
+                
+            };
+            using (var response = await client.SendAsync(groupRequest))
+            {
+                response.EnsureSuccessStatusCode();
+                var groupResponse = await response.Content.ReadAsStringAsync();
+                Groups groups = JsonConvert.DeserializeObject<Groups>(groupResponse);
+                var group = groups.Results.Where<GroupResult>(item => item.Title == groupName).FirstOrDefault();
+                groupId = group.Id;                
+            }
+            var collectionRequest = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(client.BaseAddress,$"accounts/{accountId}/groups/{groupId}/collections")
+            };
+            using (var response = await client.SendAsync(collectionRequest))
+            {
+                response.EnsureSuccessStatusCode();
+                var collectionResponse = await response.Content.ReadAsStringAsync();
+                ParamCollection paramCollection = JsonConvert.DeserializeObject<ParamCollection>(collectionResponse);
+                var collection = paramCollection.Results.Where<CollectionResult>(item => item.Title == collectionName).FirstOrDefault();
+                collectionId = collection.Id;                
+            }
+            var parameterRequest = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(client.BaseAddress,$"accounts/{accountId}/groups/{groupId}/collections/{collectionId}/parameters")
+            };
+
+            using (var response = await client.SendAsync(parameterRequest))
+            {
+                response.EnsureSuccessStatusCode();
+                var collectionResponse = await response.Content.ReadAsStringAsync();                
+                Parameters parameters = JsonConvert.DeserializeObject<Parameters>(collectionResponse);
+                parametersList = new List<ParametersResult>(parameters.Results);                
+            }
+            return parametersList;
+
+        }
+    }
+
+    public class Pagination
+    {
+        public int Offset { get; set; }
+        public int Limit { get; set; }
+        public string NextUrl { get; set; }
+    }
+
+    public class GroupResult
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class Groups
+    {
+        public Pagination Pagination { get; set; }
+        public List<GroupResult> Results { get; set; }
+    }
+
+    public class Account
+    {
+        public string Id { get; set; }
+    }
+
+    public class Group
+    {
+        public string Id { get; set; }
+    }
+
+    public class CollectionResult
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Group Group { get; set; }
+        public Account Account { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class ParamCollection
+    {
+        public Pagination Pagination { get; set; }
+        public List<CollectionResult> Results { get; set; }
+    }
+
+    public class Metadata
+    {
+        public string Id { get; set; }
+        public object Value { get; set; }
+    }
+
+    public class ParametersResult
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool ReadOnly { get; set; }
+        public string Description { get; set; }
+        public string SpecId { get; set; }
+        public List<Metadata> Metadata { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class Parameters
+    {
+        public Pagination Pagination { get; set; }
+        public List<ParametersResult> Results { get; set; }
+    }
+}
+
